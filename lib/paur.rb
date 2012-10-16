117a9f1dc60dbb4b0aafd2b47c3b8b14c7292390
patrick brisbin <pbrisbin@gmail.com>
2012-10-16 12:48:35 -0400

    sketch out Main runner

--- lib/paur.rb
+++ lib/paur.rb
@@ -1,5 +1,23 @@
-require "paur/version"
+require 'paur/utils'
+require 'paur/aur'
+require 'paur/config'
+require 'paur/tar'
+require 'paur/version'
+require 'fileutils'
 
 module Paur
-  # Your code goes here...
+  class Main
+    include Utils
+    include FileUtils
+
+    def run(config, verbose)
+      execute("makepkg -g >> ./PKGBUILD", verbose)
+      execute("#{config.editor} ./PKGBUILD", verbose)
+
+      rm_r 'src', :verbose => verbose
+
+      taurball = Tar.new(Dir.pwd, verbose).compress(config.excludes)
+      Aur.new(taurball, config.category, verbose).upload
+    end
+  end
 end
